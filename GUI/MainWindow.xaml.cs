b024ed3a8e0c6821095d743da50807e7ed56e350
compil3 <luca.tenuta@gmail.com>
2017-12-18 12:48:35 -0500

    Added initial GUI windows and code behind.

---
+++ GUI/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GUI.QuitAbout;
+
+namespace GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            var mainExit = new Quit();
+            Close();
+            mainExit.Show();
+        }
+
+        private void Show_About(object sender, RoutedEventArgs e)
+        {
+            var about = new About();
+            about.Show();
+        }
+
+        private void Show_Settings(object sender, RoutedEventArgs e)
+        {
+            var settings = new Settings.Settings();
+            Close();
+            settings.Show();
+        }
+    }
+}
---
+++ GUI/QuitAbout/About.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace GUI.QuitAbout
+{
+    /// <summary>
+    /// Interaction logic for About.xaml
+    /// </summary>
+    public partial class About : Window
+    {
+        public About()
+        {
+            InitializeComponent();
+        }
+
+        private void AboutOk(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
